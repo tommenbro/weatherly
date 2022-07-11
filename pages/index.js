fa30eb6321ea64575bb2eea5b71d8b4e5c9b03e5
Tomas Nilsen Nakkerud <74247764+tommenbro@users.noreply.github.com>
2022-07-11 02:53:35 +0200

    init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Weatherly</title>
+        <meta name="description" content="Weatherly - Nakkerud Studio, 2022" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
